0878e51dbc2a14ae06f0e5cad81c4e775f94f5ef
XueyiGu <xueyigu1990@gmail.com>
2016-04-20 00:16:52 -0700

    [343. Integer Break][Accepted]committed by LeetCode Extension

--- 343-Integer-Break/solution.java
+++ 343-Integer-Break/solution.java
@@ -1,5 +1,11 @@
 public class Solution {
-    public int integerBreak(int n) {
+    
+    public int integerBreak(int n){
+        if(n <= 3) return n-1; //assuming n >= 2
+        return n%3 == 0 ? (int)Math.pow(3, n/3) : n%3 == 1 ? (int)Math.pow(3, n/3-1)*4 : (int)Math.pow(3, n/3)*2;
+    
+    }
+    public int integerBreak1(int n) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         map.put(1, 1);
         int current = 0;
