1cdd730987a4f918155004df452d0af637152f8e
XueyiGu <xueyigu1990@gmail.com>
2016-05-26 19:36:28 -0700

    [42. Trapping Rain Water][Accepted]committed by LeetCode Extension

--- 42-Trapping-Rain-Water/solution.java
+++ 42-Trapping-Rain-Water/solution.java
@@ -2,52 +2,34 @@ public class Solution {
     public int trap(int[] height) 
     {
         
-        if(height == null || height.length < 3)
-        {
+        if(height == null || height.length < 3){
             return 0;
         }
-        
         int n = height.length;
-        
-        int[] left = new int[n];
-        int[] right = new int[n];
-        
-        //from left to right;
-        int max = height[0];
-        left[0] = height[0];
-        for(int i = 1; i < n; i++)
-        {
-            if(height[i] < max)
-            {
-                left[i] = max;
-            }
-            else
-            {
-                left[i] = height[i];
-                max = height[i];
-            }
-        }
-        
-        // from right to left
-        max = height[n -1];
-        right[n - 1] = max;
-        for(int i = n - 2; i > -1; i--)
-        {
-            if(height[i] < max)
-            {
-                right[i] = max;
-            }
-            else
-            {
-                right[i] = height[i];
-                max = height[i];
+        int maxIndex = 0;
+        for(int i = 1; i < n; i++){
+            if(height[i] > height[maxIndex]){
+                maxIndex = i;
             }
         }
         
         int water = 0;
-        for(int i = 0; i < n; i++)
-        {
-            water += Math.min(left[i], right[i]) - height[i];
+        int peak = 0;
+        for(int i = 0; i < maxIndex; i++){
+            if(height[i] > peak){
+                peak = height[i];
+            }else{
+                water += peak - height[i];
+            }
+        }
+        
+        peak = 0;
+        for(int i = n - 1; i > maxIndex; i--){
+            if(height[i] > peak){
+                peak = height[i];
+            }else{
+                water += peak - height[i];
+            }
         }
         return water;
     
