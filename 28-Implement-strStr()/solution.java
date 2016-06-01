baf9e4e1386f32dc8e4cefaecb672f29ad83186d
XueyiGu <xueyigu1990@gmail.com>
2016-06-01 12:06:21 -0700

    [28. Implement strStr()][Accepted]committed by LeetCode Extension

--- 28-Implement-strStr()/solution.java
+++ 28-Implement-strStr()/solution.java
@@ -1,32 +1,29 @@
 public class Solution {
-    public int strStr(String haystack, String needle) 
-    {
-        if(needle == null || needle.length() == 0){
-            return 0;
-        } 
-        
-        if(haystack.length() < needle.length()){
+    public int strStr(String haystack, String needle){
+        if(haystack == null || haystack.length() < needle.length()){
             return -1;
         }
         
-        for(int i = 0; i < haystack.length(); i++){
-            if(haystack.charAt(i) == needle.charAt(0)){
-                if(match(haystack, i, needle)){
-                    return i;
-                }
+        if(needle.length() == 0){
+            return 0;
+        }
+        
+        for(int i = 0; i <= haystack.length() - needle.length(); i++){
+            if(haystack.charAt(i) == needle.charAt(0) && match(haystack, needle, i)){
+                return i;
             }
         }
         return -1;
     }
     
-    private boolean match(String target, int i, String needle){
-        if(target.length() - i < needle.length()){
-            return false;
-        }
-        
-        for(int j = 0; j < needle.length(); j++){
-            if(target.charAt(i + j) != needle.charAt(j)){
+    private boolean match(String haystack, String needle, int i){
+        int j = 0;
+        while(j < needle.length()){
+            if(haystack.charAt(i) != needle.charAt(j)){
                 return false;
+            }else{
+                i++;
+                j++;
             }
         }
         return true;
