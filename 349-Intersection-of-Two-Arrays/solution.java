24b29fd072f95b5c935896192c1a15ef60c2f03a
XueyiGu <xueyigu1990@gmail.com>
2016-06-06 14:18:12 -0700

    [349. Intersection of Two Arrays][Accepted]committed by LeetCode Extension

--- 349-Intersection-of-Two-Arrays/solution.java
+++ 349-Intersection-of-Two-Arrays/solution.java
@@ -1,5 +1,11 @@
 public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums1.length == 0){
+            return new int[0];
+        }
+        if(nums2 == null || nums2.length == 0){
+            return new int[0];
+        }
         HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
         for(int i = 0; i < nums1.length; i++){
             if(!map.containsKey(nums1[i])){
@@ -7,20 +13,23 @@ public class Solution {
             }
         }
         
-        int count = 0;
         for(int i = 0; i < nums2.length; i++){
-            if(map.containsKey(nums2[i]) && map.get(nums2[i]) == false){
-                count++;
+            if(map.containsKey(nums2[i])){
                 map.put(nums2[i], true);
             }
         }
         
+        int count = 0;
+        for(int i : map.keySet()){
+            if(map.get(i) == true){
+                count++;
+            }
+        }
         int[] result = new int[count];
-        int i = 0;
-        for(Integer key : map.keySet()){
-            if(map.get(key) == true){
-                result[i] = key;
-                i++;
+        int j = 0;
+        for(int i : map.keySet()){
+            if(map.get(i)){
+                result[j++] = i;
             }
         }
         return result;
